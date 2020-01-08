1a3bb904b197e92ef87ce2c3c545a0f5236595bc
U-taro-ogw <yuutarou0406@gmail.com>
2020-01-09 01:05:22 +0900

    dbとmodelを別ファイルへ

--- main.go
+++ main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"fmt"
+	"github.com/U-taro-ogw/auth_api/src/db"
+	"github.com/U-taro-ogw/auth_api/src/models"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"net/http"
-	"os"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
 
-	db := gormConnect()
-	defer db.Close()
+	dbCon := db.DbConnect()
 	dbHandler := DbHandler{
-		Db: db,
+		Db: dbCon,
 	}
 
 	d := gin.Default()
@@ -26,34 +26,12 @@ func main() {
 	d.Run(":8083")
 }
 
-func gormConnect() *gorm.DB {
-	USER := os.Getenv("MYSQL_USER")
-	PASS := os.Getenv("MYSQL_ROOT_PASSWORD")
-	PROTOCOL := "tcp(db:3306)"
-	DBNAME := os.Getenv("MYSQL_DATABASE")
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
-	db, err := gorm.Open("mysql", CONNECT)
-
-	if err != nil {
-		panic("データベース接続に失敗しました。")
-	}
-	db.AutoMigrate(&User{})
-
-	return db
-}
-
-type User struct {
-	gorm.Model
-	Email string `json:"e-mail"`
-	Password string `json:"password"`
-}
-
 type DbHandler struct {
 	Db *gorm.DB
 }
 
 func (h *DbHandler) Signup(c *gin.Context) {
-	user := User{}
+	user := models.User{}
 	err := c.BindJSON(&user)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -66,8 +44,8 @@ func (h *DbHandler) Signup(c *gin.Context) {
 }
 
 func (h *DbHandler) Signin(c *gin.Context) {
-	var user User
-	var findUser User
+	var user models.User
+	var findUser models.User
 	c.BindJSON(&user)
 	if err := h.Db.Where("email = ? AND password = ?", user.Email, user.Password).First(&findUser).Error; gorm.IsRecordNotFoundError(err) {
 		c.JSON(http.StatusNotFound, gin.H{"error": "NotFound"})
---
+++ src/db/db.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"github.com/U-taro-ogw/auth_api/src/models"
+	"github.com/jinzhu/gorm"
+	"os"
+)
+
+func DbConnect() *gorm.DB {
+	USER := os.Getenv("MYSQL_USER")
+	PASS := os.Getenv("MYSQL_ROOT_PASSWORD")
+	PROTOCOL := "tcp(db:3306)"
+	DBNAME := os.Getenv("MYSQL_DATABASE")
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
+	db, err := gorm.Open("mysql", CONNECT)
+
+	if err != nil {
+		panic("データベース接続に失敗しました。")
+	}
+	db.AutoMigrate(&models.User{})
+
+	defer db.Close()
+
+	return db
+}
