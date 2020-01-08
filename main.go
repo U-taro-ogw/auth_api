02b80eca762eeb67a12a2efd050756f26c512b17
U-taro-ogw <yuutarou0406@gmail.com>
2020-01-09 01:29:16 +0900

    handlerを別ファイルに

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/U-taro-ogw/auth_api/src/db"
-	"github.com/U-taro-ogw/auth_api/src/models"
+	"github.com/U-taro-ogw/auth_api/src/handlers"
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"net/http"
+
 )
 
 func main() {
@@ -17,7 +16,7 @@ func main() {
 	defer dbCon.Close()
 	dbCon.LogMode(true)
 
-	dbHandler := DbHandler{
+	dbHandler := handlers.UserHandler{
 		Db: dbCon,
 	}
 
@@ -27,36 +26,4 @@ func main() {
 	d.POST("/signin", dbHandler.Signin)
 
 	d.Run(":8083")
-}
-
-type DbHandler struct {
-	Db *gorm.DB
-}
-
-func (h *DbHandler) Signup(c *gin.Context) {
-	user := models.User{}
-	err := c.BindJSON(&user)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	}
-
-	h.Db.NewRecord(user)
-	h.Db.Create(&user)
-
-	c.JSON(200, gin.H{"message": "signup"})
-}
-
-func (h *DbHandler) Signin(c *gin.Context) {
-	var user models.User
-	var findUser models.User
-	c.BindJSON(&user)
-	fmt.Println("-------------------------====>>>>>>>>>>>>>")
-	fmt.Println(user)
-	fmt.Println(h.Db.Where("email = ? AND password = ?", user.Email, user.Password))
-
-	if err := h.Db.Where("email = ? AND password = ?", user.Email, user.Password).First(&findUser).Error; gorm.IsRecordNotFoundError(err) {
-		c.JSON(http.StatusNotFound, gin.H{"error": "NotFound"})
-	} else {
-		c.JSON(http.StatusOK, gin.H{"message": "Find!!"})
-	}
 }
--- src/handlers/user.go
+++ src/handlers/user.go
@@ -1,1 +1,37 @@
 package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"github.com/U-taro-ogw/auth_api/src/models"
+	"net/http"
+)
+
+type UserHandler struct {
+	Db *gorm.DB
+}
+
+func (h *UserHandler) Signup(c *gin.Context) {
+	user := models.User{}
+	err := c.BindJSON(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	h.Db.NewRecord(user)
+	h.Db.Create(&user)
+
+	c.JSON(200, gin.H{"message": "signup"})
+}
+
+func (h *UserHandler) Signin(c *gin.Context) {
+	var userParam models.User
+	var findUser models.User
+	c.BindJSON(&userParam)
+
+	if err := h.Db.Where("email = ? AND password = ?", userParam.Email, userParam.Password).First(&findUser).Error; gorm.IsRecordNotFoundError(err) {
+		c.JSON(http.StatusNotFound, gin.H{"error": "NotFound"})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"message": "Find!!"})
+	}
+}
